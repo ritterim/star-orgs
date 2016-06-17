57b23b56df1cbe5731b9dc05f2a3e2847843d44f
Ken Dale <ken@kendaleiv.com>
2016-06-17 08:49:36 -0400

    Add filterFunction test

--- test/directory.tests.js
+++ test/directory.tests.js
@@ -3,7 +3,11 @@ import fetchMock from 'fetch-mock';
 
 import Directory from '../src/directory';
 
-test('Calls directoryUrl', t => {
+test.afterEach(() => {
+  fetchMock.restore();
+});
+
+test.serial('Calls directoryUrl', t => {
   const testDirectoryUrl = 'directory-test-url';
 
   fetchMock.mock(testDirectoryUrl, { });
@@ -12,5 +16,19 @@ test('Calls directoryUrl', t => {
     .getUsers(testDirectoryUrl)
     .then(() => {
       t.true(fetchMock.called(testDirectoryUrl));
+    });
+});
+
+test.serial('Uses filterFunction when supplied', t => {
+  const testDirectoryUrl = 'directory-test-url';
+  const testFilterFunction = x => x.test === 'b';
+
+  fetchMock.mock(testDirectoryUrl, [{ test: 'a' }, { test: 'b' }]);
+
+  return new Directory()
+    .getUsers(testDirectoryUrl, testFilterFunction)
+    .then(users => {
+      t.is(users.length, 1);
+      t.is(users[0].test, 'b');
     });
 });
