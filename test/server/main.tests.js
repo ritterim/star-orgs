695de454d4587cd76edaa001cd37b34d66af27e6
Ken Dale <ken@kendaleiv.com>
2016-06-27 17:26:41 -0400

    Add server/main tests

---
+++ test/server/main.tests.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-magic-numbers */
+
+import test from 'ava';
+const proxyquire = require('proxyquire').noCallThru();
+
+export class TestAccessTokenRetriever {
+  getAccessToken() {
+    return Promise.resolve('test_access_token');
+  }
+}
+
+export class TestUsersRetriever {
+  getUsers() {
+    return Promise.resolve([{ id: 'test-id' }]);
+  }
+}
+
+export class AccessTokenCheckingFakeWindowsGraphUsersRetriever {
+  getUsers(endpointId, accessToken /* , uriOverride */) {
+    if (accessToken !== 'test_access_token') {
+      throw new Error('Access token did not match expected value.');
+    }
+
+    return Promise.resolve([{ id: 'test-id' }]);
+  }
+}
+
+let testWebServerStarted = false;
+
+export class TestWebServer {
+  constructor() {
+    testWebServerStarted = false;
+  }
+
+  start() {
+    testWebServerStarted = true;
+  }
+}
+
+process.env.port = 0;
+
+test.beforeEach(() => {
+  delete process.env.ENDPOINT_ID;
+});
+
+test('start should start web server with directory items', t => {
+  const Main = proxyquire(
+    '../../src/server/main',
+    { './web-server': TestWebServer }
+  ).default;
+  const main = new Main();
+
+  return main.start()
+    .then(() => {
+      t.true(testWebServerStarted);
+    });
+});
+
+test('refreshData should use RandomUsersRetriever when process.env.ENDPOINT_ID is not set', t => {
+  const Main = proxyquire(
+    '../../src/server/main',
+    { './random-users-retriever': TestUsersRetriever }
+  ).default;
+  const main = new Main();
+
+  return main.refreshData()
+    .then(() => {
+      t.is(main.directoryItems.length, 1);
+      t.is(main.directoryItems[0].id, 'test-id');
+    });
+});
+
+test('refreshData should use WindowsGraphUsersRetriever when process.env.ENDPOINT_ID is set', t => {
+  process.env.ENDPOINT_ID = 'test';
+
+  const Main = proxyquire(
+    '../../src/server/main',
+    {
+      './access-token-retriever': TestAccessTokenRetriever,
+      './windows-graph-users-retriever': TestUsersRetriever
+    }
+  ).default;
+  const main = new Main();
+
+  return main.refreshData()
+    .then(() => {
+      t.is(main.directoryItems.length, 1);
+      t.is(main.directoryItems[0].id, 'test-id');
+    });
+});
+
+test('refreshData should provide correct access token to WindowsGraphUsersRetriever', t => {
+  process.env.ENDPOINT_ID = 'test';
+
+  const Main = proxyquire(
+    '../../src/server/main',
+    {
+      './access-token-retriever': TestAccessTokenRetriever,
+      './windows-graph-users-retriever': AccessTokenCheckingFakeWindowsGraphUsersRetriever
+    }
+  ).default;
+  const main = new Main();
+
+  // If AccessTokenCheckingFakeWindowsGraphUsersRetriever throws
+  // main.directoryItems should not be set.
+  return main.refreshData()
+    .then(() => {
+      t.is(main.directoryItems.length, 1);
+      t.is(main.directoryItems[0].id, 'test-id');
+    });
+});
