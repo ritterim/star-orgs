146e5692ceade6b353850838b5ef79e4f9f67169
Ken Dale <ken@kendaleiv.com>
2016-07-05 09:47:27 -0400

    Remove CSS concerns from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,16 +20,7 @@ module.exports = {
         query: {
           presets: ['es2015']
         }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-
-      // These loaders adapted from
-      // https://github.com/gowravshekar/font-awesome-webpack/tree/48a9151238e6ebe382301512a48183a7ae057f62#usage
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?name=lib/[name]&limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[hash].[ext]' }
+      }
     ]
   },
   resolveLoader: {
