37eb06b19813eb01b587ff2e32fb64f1c2e0841b
Ken Dale <ken@kendaleiv.com>
2016-07-05 14:09:52 -0400

    Allow configuration of image retriever

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,5 @@
 import Directory from './directory';
+import GravatarImageRetriever from './gravatar-image-retriever';
 import ForceDirectedGraphRenderer from './force-directed-graph-renderer';
 import Office365GetPersonaPhotoImageRetriever from './office365-get-persona-photo-image-retriever';
 
@@ -7,7 +8,10 @@ const directoryUrl = 'directory';
 const filterFunction = x => x.department;
 
 const directory = new Directory();
-const imageRetriever = new Office365GetPersonaPhotoImageRetriever();
+const imageRetriever = process.env.IMAGE_RETRIEVER === 'Office365GetPersonaPhotoImageRetriever'
+  ? new Office365GetPersonaPhotoImageRetriever()
+  : new GravatarImageRetriever(150); // eslint-disable-line no-magic-numbers
+
 const renderer = new ForceDirectedGraphRenderer(
   containerElement,
   imageRetriever);
