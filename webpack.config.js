288fb2a26adc5148c9cd255629c7ddfc9664fe04
Ken Dale <ken@kendaleiv.com>
2016-07-05 08:20:09 -0400

    Remove extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -24,16 +23,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: 'style-loader!css-loader'
       },
 
+      // These loaders adapted from
       // https://github.com/gowravshekar/font-awesome-webpack/tree/48a9151238e6ebe382301512a48183a7ae057f62#usage
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?name=lib/[name]&limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[hash].[ext]' }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('bundle.css')
-  ],
   devTool: 'source-map'
 };
